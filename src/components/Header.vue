f66f70c572c406e035eab620895741a6aeeffc8b
kevin-tian <kevin.tian@ibm.com>
2023-12-24 11:45:05 -0500

    fix dark mode & add tooltip

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref, watch } from "vue";
+import { onMounted, ref, watch, computed } from "vue";
 import { useStore } from "../store/store";
 const store = useStore();
 const systemDarkMode = window.matchMedia(
@@ -22,12 +22,16 @@ watch(clicked, () => {
   store.updateDarkMode(clicked.value);
 });
 
+const darkMode = computed(() => {
+  return store.darkMode;
+});
+
 const setColorMode = () => {
   clicked.value = !clicked.value;
 };
 </script>
 <template>
-  <nav class="bg-blue-200 border-gray-200 rounded-b-xl dark:bg-gray-900">
+  <nav class="bg-blue-200 border-gray-200 rounded-b-xl dark:bg-gray-700">
     <div
       class="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4"
     >
@@ -47,24 +51,16 @@ const setColorMode = () => {
         <div
           class="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0"
         >
-          <!-- <svg
-            class="w-6 h-6 text-gray-800 dark:text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 18 20"
-          >
-            <path
-              d="M17.8 13.75a1 1 0 0 0-.859-.5A7.488 7.488 0 0 1 10.52 2a1 1 0 0 0 0-.969A1.035 1.035 0 0 0 9.687.5h-.113a9.5 9.5 0 1 0 8.222 14.247 1 1 0 0 0 .004-.997Z"
-            />
-          </svg> -->
           <button
             @click="setColorMode()"
+            data-tooltip-target="tooltip-bottom"
+            data-tooltip-placement="bottom"
             type="button"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            class="text-white bg-blue-200 hover:bg-blue-300 focus:ring focus:outline-none focus:ring-blue-300 font-small rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-600"
           >
             <svg
-              class="w-6 h-6 text-gray-800 dark:text-white"
+              v-if="darkMode"
+              class="w-4 h-4 text-gray-800 dark:text-white"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
@@ -74,9 +70,27 @@ const setColorMode = () => {
                 d="M10 15a5 5 0 1 0 0-10 5 5 0 0 0 0 10Zm0-11a1 1 0 0 0 1-1V1a1 1 0 0 0-2 0v2a1 1 0 0 0 1 1Zm0 12a1 1 0 0 0-1 1v2a1 1 0 1 0 2 0v-2a1 1 0 0 0-1-1ZM4.343 5.757a1 1 0 0 0 1.414-1.414L4.343 2.929a1 1 0 0 0-1.414 1.414l1.414 1.414Zm11.314 8.486a1 1 0 0 0-1.414 1.414l1.414 1.414a1 1 0 0 0 1.414-1.414l-1.414-1.414ZM4 10a1 1 0 0 0-1-1H1a1 1 0 0 0 0 2h2a1 1 0 0 0 1-1Zm15-1h-2a1 1 0 1 0 0 2h2a1 1 0 0 0 0-2ZM4.343 14.243l-1.414 1.414a1 1 0 1 0 1.414 1.414l1.414-1.414a1 1 0 0 0-1.414-1.414ZM14.95 6.05a1 1 0 0 0 .707-.293l1.414-1.414a1 1 0 1 0-1.414-1.414l-1.414 1.414a1 1 0 0 0 .707 1.707Z"
               />
             </svg>
-
-            <span class="sr-only">Icon description</span>
+            <svg
+              v-else
+              class="w-4 h-4 text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 18 20"
+            >
+              <path
+                d="M17.8 13.75a1 1 0 0 0-.859-.5A7.488 7.488 0 0 1 10.52 2a1 1 0 0 0 0-.969A1.035 1.035 0 0 0 9.687.5h-.113a9.5 9.5 0 1 0 8.222 14.247 1 1 0 0 0 .004-.997Z"
+              />
+            </svg>
           </button>
+          <div
+            id="tooltip-bottom"
+            role="tooltip"
+            class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-600"
+          >
+            Toggle dark mode
+            <div class="tooltip-arrow" data-popper-arrow></div>
+          </div>
         </div>
       </div>
     </div>
