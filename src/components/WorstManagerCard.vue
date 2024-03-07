9e4f6427889eb659e9d49908f077dc9f78d43330
kt474 <kt474@cornell.edu>
2024-03-06 20:51:06 -0500

    Fix types imports

--- src/components/WorstManagerCard.vue
+++ src/components/WorstManagerCard.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { computed } from "vue";
-import { RosterType } from "../store/store";
+import { RosterType } from "../api/types";
 const props = defineProps<{
   users: any[];
   rosters: any[];
