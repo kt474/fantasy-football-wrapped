71a6aff7464721ad56c3f48c387318528dc69f69
kt474 <kt474@cornell.edu>
2024-06-14 22:03:07 -0400

    Fix loading page formatting

--- src/components/SkeletonLoading.vue
+++ src/components/SkeletonLoading.vue
@@ -4,7 +4,7 @@
   <div class="flex w-full">
     <div
       role="status"
-      class="p-4 space-y-4 w-3/4 mr-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700"
+      class="w-full p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow lg:mr-4 lg:w-3/4 animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700"
     >
       <div class="flex items-center justify-between">
         <div>
@@ -26,7 +26,7 @@
       </div>
       <span class="sr-only">Loading...</span>
     </div>
-    <div class="w-1/4">
+    <div class="hidden w-1/4 lg:block">
       <div
         role="status"
         class="p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700"
@@ -43,16 +43,16 @@
             class="w-full bg-gray-200 rounded-t-lg h-28 dark:bg-gray-700"
           ></div>
           <div
-            class="w-full h-28 ms-6 bg-gray-200 rounded-t-lg dark:bg-gray-700"
+            class="w-full bg-gray-200 rounded-t-lg h-28 ms-6 dark:bg-gray-700"
           ></div>
           <div
             class="w-full bg-gray-200 rounded-t-lg h-36 ms-6 dark:bg-gray-700"
           ></div>
           <div
-            class="w-full h-40 ms-6 bg-gray-200 rounded-t-lg dark:bg-gray-700"
+            class="w-full h-40 bg-gray-200 rounded-t-lg ms-6 dark:bg-gray-700"
           ></div>
           <div
-            class="w-full bg-gray-200 rounded-t-lg h-32 ms-6 dark:bg-gray-700"
+            class="w-full h-32 bg-gray-200 rounded-t-lg ms-6 dark:bg-gray-700"
           ></div>
           <div
             class="w-full bg-gray-200 rounded-t-lg h-36 ms-6 dark:bg-gray-700"
@@ -76,19 +76,19 @@
 
         <div class="flex items-baseline mt-4">
           <div
-            class="w-full bg-gray-200 rounded-t-lg h-24 dark:bg-gray-700"
+            class="w-full h-24 bg-gray-200 rounded-t-lg dark:bg-gray-700"
           ></div>
           <div
-            class="w-full h-32 ms-6 bg-gray-200 rounded-t-lg dark:bg-gray-700"
+            class="w-full h-32 bg-gray-200 rounded-t-lg ms-6 dark:bg-gray-700"
           ></div>
           <div
-            class="w-full bg-gray-200 rounded-t-lg h-32 ms-6 dark:bg-gray-700"
+            class="w-full h-32 bg-gray-200 rounded-t-lg ms-6 dark:bg-gray-700"
           ></div>
           <div
-            class="w-full h-44 ms-6 bg-gray-200 rounded-t-lg dark:bg-gray-700"
+            class="w-full bg-gray-200 rounded-t-lg h-44 ms-6 dark:bg-gray-700"
           ></div>
           <div
-            class="w-full bg-gray-200 rounded-t-lg h-32 ms-6 dark:bg-gray-700"
+            class="w-full h-32 bg-gray-200 rounded-t-lg ms-6 dark:bg-gray-700"
           ></div>
           <div
             class="w-full bg-gray-200 rounded-t-lg h-52 ms-6 dark:bg-gray-700"
