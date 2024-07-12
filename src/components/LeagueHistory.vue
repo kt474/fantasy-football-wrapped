310a19cec5df818fa5eeca248479f84cd0fdfdb9
kt474 <kt474@cornell.edu>
2024-07-12 02:01:12 -0400

    add seasons column

--- src/components/LeagueHistory.vue
+++ src/components/LeagueHistory.vue
@@ -11,6 +11,7 @@ const props = defineProps<{
 }>();
 
 const isLoading = ref(false);
+const tableOrder = ref("wins");
 
 const checkPreviousLeagues = async (leagueId: string) => {
   const leagueData = await getData(leagueId);
@@ -53,6 +54,7 @@ const dataAllYears = computed(() => {
       losses: user.losses,
       points: user.pointsFor,
       avatarImg: user.avatarImg,
+      seasons: [store.leagueInfo[store.currentLeagueIndex].season],
     });
   });
   if (store.leagueInfo[store.currentLeagueIndex].previousLeagues.length > 0) {
@@ -64,6 +66,9 @@ const dataAllYears = computed(() => {
               resultUser.wins += user.wins;
               resultUser.losses += user.losses;
               resultUser.points += user.pointsFor;
+              if (league.weeklyPoints.length > 0) {
+                resultUser.seasons.push(league.season);
+              }
             }
           });
         });
@@ -71,6 +76,21 @@ const dataAllYears = computed(() => {
     );
   }
   return result;
+});
+
+const tableData = computed(() => {
+  if (tableOrder.value === "wins") {
+    return dataAllYears.value.sort((a: any, b: any) => {
+      if (a.wins !== b.wins) {
+        return b.wins - a.wins;
+      }
+      return b.points - a.points;
+    });
+  } else if (tableOrder.value === "points") {
+    return dataAllYears.value.sort((a: any, b: any) => {
+      return b.points - a.points;
+    });
+  }
 });
 </script>
 <template>
@@ -90,55 +110,45 @@ const dataAllYears = computed(() => {
         <tr>
           <th scope="col" class="px-6 py-3">Team name</th>
           <th scope="col" class="px-6 py-3">
-            <div class="flex items-center">
-              Wins
-              <a href="#"
-                ><svg
-                  class="w-3 h-3 ms-1.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"
-                  /></svg
-              ></a>
+            <div
+              class="flex items-center cursor-pointer"
+              @click="tableOrder = 'wins'"
+            >
+              Compiled Record
+              <svg
+                class="w-3 h-3 ms-1.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"
+                />
+              </svg>
             </div>
           </th>
           <th scope="col" class="px-6 py-3">
-            <div class="flex items-center">
-              Losses
-              <a href="#"
-                ><svg
-                  class="w-3 h-3 ms-1.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"
-                  /></svg
-              ></a>
-            </div>
-          </th>
-          <th scope="col" class="px-6 py-3">
-            <div class="flex items-center">
+            <div
+              class="flex items-center cursor-pointer"
+              @click="tableOrder = 'points'"
+            >
               Points
-              <a href="#"
-                ><svg
-                  class="w-3 h-3 ms-1.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"
-                  /></svg
-              ></a>
+              <svg
+                class="w-3 h-3 ms-1.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"
+                />
+              </svg>
             </div>
+          </th>
+          <th scope="col" class="px-6 py-3">
+            <div class="flex items-center">Seasons</div>
           </th>
           <th scope="col" class="px-6 py-3">
             <span class="sr-only">Edit</span>
@@ -147,7 +157,7 @@ const dataAllYears = computed(() => {
       </thead>
       <tbody>
         <tr
-          v-for="(user, index) in dataAllYears"
+          v-for="(user, index) in tableData"
           class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
         >
           <th
@@ -177,31 +187,35 @@ const dataAllYears = computed(() => {
               <p>{{ user.name }}</p>
             </div>
           </th>
-          <td class="px-6 py-4">{{ user.wins }}</td>
-          <td class="px-6 py-4">{{ user.losses }}</td>
+          <td class="px-6 py-4">{{ user.wins }} - {{ user.losses }}</td>
           <td class="px-6 py-4">{{ user.points }}</td>
+          <td class="px-6 py-4">{{ user.seasons.join(", ") }}</td>
           <td class="px-6 py-4 text-right"></td>
         </tr>
       </tbody>
     </table>
   </div>
-  <svg
-    v-else
-    aria-hidden="true"
-    class="w-8 h-8 mx-auto mt-4 mb-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-    viewBox="0 0 100 101"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-      fill="currentColor"
-    />
-    <path
-      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-      fill="currentFill"
-    />
-  </svg>
+  <div v-else>
+    <svg
+      aria-hidden="true"
+      class="w-8 h-8 mx-auto mt-4 mb-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+      viewBox="0 0 100 101"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+        fill="currentColor"
+      />
+      <path
+        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+        fill="currentFill"
+      />
+    </svg>
+    <p class="flex justify-center text-lg dark:text-white">
+      Loading previous leagues...
+    </p>
+  </div>
 </template>
 <style scoped>
 .light-custom-bg-color {
