eee08400d276f2948f5761bf4e47be7d268ecda3
kt474 <kt474@cornell.edu>
2024-09-11 08:34:04 -0400

    Update homepage text

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -2,20 +2,19 @@
 <template>
   <div class="pt-20 text-center">
     <h1
-      class="px-1 mb-4 font-bold tracking-tight text-center text-gray-900 custom-font-size sm:leading-none lg:px-40 xl:px-56 md:text-5xl lg:text-6xl dark:text-white"
+      class="px-1 font-bold tracking-tight text-center text-gray-900 custom-font-size sm:leading-none lg:px-24 xl:px-56 md:text-5xl lg:text-6xl dark:text-white"
     >
-      Your complete
+      All your
       <span
         class="text-transparent bg-clip-text bg-gradient-to-r to-indigo-600 from-sky-500 dark:to-indigo-500 dark:from-sky-400"
         >fantasy football</span
       >
-      league summary
+      league insights in one place
     </h1>
     <p
       class="max-w-4xl mx-auto mt-6 mb-0 text-lg font-normal text-center text-gray-500 text-balance lg:text-xl sm:px-16 dark:text-gray-300"
     >
-      Detailed analysis and informative charts for winning fantasy football
-      managers.
+      Informative data and elegant charts for winning fantasy football managers.
     </p>
     <p
       class="max-w-3xl mx-auto text-lg font-normal text-center text-gray-500 lg:text-xl sm:px-16 dark:text-gray-300"
