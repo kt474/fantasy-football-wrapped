781891dc6db80147740de586d37a8bb1d97ad155
kt474 <kt474@cornell.edu>
2024-09-16 22:33:02 -0400

    fix edge case if points don't exist

--- src/api/helper.ts
+++ src/api/helper.ts
@@ -27,7 +27,9 @@ export const createTableData = (
 
     const pointsArr: any[] = [];
     combinedPoints.forEach((value: any) => {
-      pointsArr.push(value.points.slice(0, value.recordByWeek.length));
+      const weekLength = value.recordByWeek ? value.recordByWeek.length : 0;
+      const pointsList = value.points ? value.points : [];
+      pointsArr.push(pointsList.slice(0, weekLength));
       value["winsAgainstAll"] = 0;
       value["lossesAgainstAll"] = 0;
     });
@@ -82,10 +84,9 @@ export const createTableData = (
           value.wins / (value.wins + value.losses)
         );
         if (!medianScoring) {
-          const pairs = zip(
-            value.points.slice(0, value.recordByWeek.length),
-            medians
-          );
+          const weekLength = value.recordByWeek ? value.recordByWeek.length : 0;
+          const pointsList = value.points ? value.points : [];
+          const pairs = zip(pointsList.slice(0, weekLength), medians);
           const counts = countBy(pairs, ([a, b]: [number, number]) => a > b);
           const addedWins = counts["true"] ? counts["true"] : 0;
           const addedLosses = counts["false"] ? counts["false"] : 0;
