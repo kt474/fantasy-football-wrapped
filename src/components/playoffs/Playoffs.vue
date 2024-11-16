9fd3ac1cd9be70b81bb260c4e585ea22af59878b
kt474 <kt474@cornell.edu>
2024-11-16 11:02:30 -0500

    Add record to playoff cards

--- src/components/playoffs/Playoffs.vue
+++ src/components/playoffs/Playoffs.vue
@@ -65,6 +65,13 @@ const getPointsScored = (rosterId: number, week: number) => {
   return pointsArray.points[week - 1];
 };
 
+const getRecord = (rosterId: number) => {
+  const user: any = props.tableData.find((val) => val.rosterId === rosterId);
+  const numWins = user.recordByWeek.split("W").length - 1;
+  const numLosses = user.recordByWeek.split("L").length - 1;
+  return `${numWins} - ${numLosses}`;
+};
+
 const finalPlacements = computed(() => {
   if (
     store.leagueInfo.length > 0 &&
@@ -235,9 +242,12 @@ const numberOfLoserRounds = computed(() => {
                         d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"
                       />
                     </svg>
-                    <p class="mx-2 truncate max-w-20 xl:max-w-32">
-                      {{ matchRosterId(matchup.t1).name }}
-                    </p>
+                    <div class="-mt-0.5 text-gray-600 dark:text-gray-300">
+                      <p class="mx-2 truncate max-w-20 xl:max-w-32">
+                        {{ matchRosterId(matchup.t1).name }}
+                      </p>
+                      <p class="ml-2 text-xs">({{ getRecord(matchup.t1) }})</p>
+                    </div>
                   </div>
                   <p
                     class="mr-1"
@@ -273,9 +283,14 @@ const numberOfLoserRounds = computed(() => {
                           d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"
                         />
                       </svg>
-                      <p class="mx-2 mt-1 truncate max-w-20 xl:max-w-32">
-                        {{ matchRosterId(matchup.t2).name }}
-                      </p>
+                      <div class="-mt-0.5 text-gray-600 dark:text-gray-300">
+                        <p class="mx-2 truncate max-w-20 xl:max-w-32">
+                          {{ matchRosterId(matchup.t2).name }}
+                        </p>
+                        <p class="ml-2 text-xs">
+                          ({{ getRecord(matchup.t2) }})
+                        </p>
+                      </div>
                     </div>
                     <p
                       class="mt-1 mr-1"
@@ -317,9 +332,12 @@ const numberOfLoserRounds = computed(() => {
                         d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"
                       />
                     </svg>
-                    <p class="mx-2 mt-0.5 truncate max-w-20 xl:max-w-32">
-                      {{ matchRosterId(matchup.t1).name }}
-                    </p>
+                    <div class="-mt-0.5 text-gray-600 dark:text-gray-300">
+                      <p class="mx-2 truncate max-w-20 xl:max-w-32">
+                        {{ matchRosterId(matchup.t1).name }}
+                      </p>
+                      <p class="ml-2 text-xs">({{ getRecord(matchup.t1) }})</p>
+                    </div>
                   </div>
                   <p
                     class="mt-0.5 mr-1"
@@ -357,9 +375,14 @@ const numberOfLoserRounds = computed(() => {
                           d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"
                         />
                       </svg>
-                      <p class="mx-2 mt-0.5 truncate max-w-20 xl:max-w-32">
-                        {{ matchRosterId(matchup.t2).name }}
-                      </p>
+                      <div class="-mt-0.5 text-gray-600 dark:text-gray-300">
+                        <p class="mx-2 truncate max-w-20 xl:max-w-32">
+                          {{ matchRosterId(matchup.t2).name }}
+                        </p>
+                        <p class="ml-2 text-xs">
+                          ({{ getRecord(matchup.t2) }})
+                        </p>
+                      </div>
                     </div>
                     <p
                       class="mt-0.5 mr-1"
@@ -492,9 +515,12 @@ const numberOfLoserRounds = computed(() => {
                         d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"
                       />
                     </svg>
-                    <p class="mx-2 truncate max-w-20 xl:max-w-32">
-                      {{ matchRosterId(matchup.t1).name }}
-                    </p>
+                    <div class="-mt-0.5 text-gray-600 dark:text-gray-300">
+                      <p class="mx-2 truncate max-w-20 xl:max-w-32">
+                        {{ matchRosterId(matchup.t1).name }}
+                      </p>
+                      <p class="ml-2 text-xs">({{ getRecord(matchup.t1) }})</p>
+                    </div>
                   </div>
                   <p
                     class="mr-1"
@@ -532,9 +558,14 @@ const numberOfLoserRounds = computed(() => {
                           d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"
                         />
                       </svg>
-                      <p class="mx-2 mt-1 truncate max-w-20 xl:max-w-32">
-                        {{ matchRosterId(matchup.t2).name }}
-                      </p>
+                      <div class="-mt-0.5 text-gray-600 dark:text-gray-300">
+                        <p class="mx-2 truncate max-w-20 xl:max-w-32">
+                          {{ matchRosterId(matchup.t2).name }}
+                        </p>
+                        <p class="ml-2 text-xs">
+                          ({{ getRecord(matchup.t2) }})
+                        </p>
+                      </div>
                     </div>
                     <p
                       class="mt-1 mr-1"
@@ -576,9 +607,12 @@ const numberOfLoserRounds = computed(() => {
                         d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"
                       />
                     </svg>
-                    <p class="mx-2 mt-0.5 truncate max-w-20 xl:max-w-32">
-                      {{ matchRosterId(matchup.t1).name }}
-                    </p>
+                    <div class="-mt-0.5 text-gray-600 dark:text-gray-300">
+                      <p class="mx-2 truncate max-w-20 xl:max-w-32">
+                        {{ matchRosterId(matchup.t1).name }}
+                      </p>
+                      <p class="ml-2 text-xs">({{ getRecord(matchup.t1) }})</p>
+                    </div>
                   </div>
                   <p
                     class="mt-0.5 mr-1"
@@ -616,9 +650,14 @@ const numberOfLoserRounds = computed(() => {
                           d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"
                         />
                       </svg>
-                      <p class="mx-2 mt-0.5 truncate max-w-20 xl:max-w-32">
-                        {{ matchRosterId(matchup.t2).name }}
-                      </p>
+                      <div class="-mt-0.5 text-gray-600 dark:text-gray-300">
+                        <p class="mx-2 truncate max-w-20 xl:max-w-32">
+                          {{ matchRosterId(matchup.t2).name }}
+                        </p>
+                        <p class="ml-2 text-xs">
+                          ({{ getRecord(matchup.t2) }})
+                        </p>
+                      </div>
                     </div>
                     <p
                       class="mt-0.5 mr-1"
