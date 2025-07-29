5be5a401fa13ab119e207f95f147bdc56bc9f873
kt474 <kt474@cornell.edu>
2025-07-29 09:04:49 -0400

    update trades button

--- src/components/roster_management/Trades.vue
+++ src/components/roster_management/Trades.vue
@@ -417,8 +417,45 @@ watch(
           v-if="tradeData.length > 6"
           @click="toggleTrades()"
           aria-label="Button to show all trades"
-          class="text-gray-50 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 sm:px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-2"
+          class="flex text-gray-900 mt-2 bg-gray-100 border border-gray-300 focus:outline-none hover:bg-gray-200 focus:ring-2 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-700 dark:text-white dark:border-gray-800 dark:hover:bg-gray-600 dark:hover:border-gray-600 dark:focus:ring-gray-600"
         >
+          <svg
+            v-if="showAllTrades"
+            class="w-5 h-5 mr-2 -ml-2 text-gray-900 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m5 15 7-7 7 7"
+            />
+          </svg>
+          <svg
+            v-else
+            class="w-5 h-5 mr-2 -ml-2 text-gray-900 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m19 9-7 7-7-7"
+            />
+          </svg>
+
           {{
             showAllTrades
               ? "Show Fewer Trades"
