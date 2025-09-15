31bafbb4db54c36107fd67d4553d706e516026a5
kt474 <kt474@cornell.edu>
2025-09-15 19:48:06 -0400

    Update contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -20,7 +20,7 @@ const subscribe = async () => {
 };
 </script>
 <template>
-  <div class="container w-11/12 h-auto max-w-screen-xl pb-20 mx-auto">
+  <div class="container w-11/12 h-screen max-w-screen-xl pb-20 mx-auto">
     <div class="container mx-auto mt-4">
       <section>
         <div class="px-4 py-8">
@@ -42,13 +42,13 @@ const subscribe = async () => {
                   v-model="email"
                   type="email"
                   placeholder="Your email"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-50 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-50 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   required
                 />
               </div>
               <button
                 type="submit"
-                class="px-4 py-2.5 ml-2 text-gray-50 text-sm bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-2 focus:ring-blue-300 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                class="px-4 py-2.5 ml-2 text-gray-50 text-base bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-2 focus:ring-blue-300 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Subscribe
               </button>
@@ -57,7 +57,7 @@ const subscribe = async () => {
           <button
             aria-label="Button to go back one page"
             @click="router.back()"
-            class="text-gray-50 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-3 sm:px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-4"
+            class="text-gray-50 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base w-full sm:w-auto px-3 sm:px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-4"
           >
             Back
           </button>
