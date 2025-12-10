5788acbaa23b44c24952708dcdbf28b8fb68fb94
kt474 <kt474@cornell.edu>
2025-12-10 09:29:38 -0500

    Update weekly preview summary prompt

--- src/components/weekly_report/PreviewSummary.vue
+++ src/components/weekly_report/PreviewSummary.vue
@@ -52,6 +52,28 @@ const promptData = computed(() => {
     currentRanking: props.matchup2.regularSeasonRank,
   };
 
+  const playoffMatchup =
+    store.leagueInfo[store.currentLeagueIndex]?.currentWeek >
+    store.leagueInfo[store.currentLeagueIndex]?.regularSeasonLength;
+
+  if (playoffMatchup) {
+    return {
+      leagueMetadata: {
+        regularSeasonLength:
+          store.leagueInfo[store.currentLeagueIndex]?.regularSeasonLength,
+        playoffTeams: store.leagueInfo[store.currentLeagueIndex]?.playoffTeams,
+        currentWeek: store.leagueInfo[store.currentLeagueIndex]?.currentWeek,
+        playoffMatchup: playoffMatchup,
+        playoffBracket:
+          props.matchup1.regularSeasonRank <=
+          store.leagueInfo[store.currentLeagueIndex]?.playoffTeams
+            ? "winners"
+            : "losers",
+      },
+      team1,
+      team2,
+    };
+  }
   return {
     leagueMetadata: {
       regularSeasonLength:
@@ -65,14 +87,14 @@ const promptData = computed(() => {
 });
 </script>
 <template>
-  <div class="bg-gray-50 mt-3 rounded dark:bg-gray-700 mx-0 sm:mx-2 p-4">
+  <div class="p-4 mx-0 mt-3 rounded bg-gray-50 dark:bg-gray-700 sm:mx-2">
     <h3 class="font-semibold text-gray-800 dark:text-gray-50">
       Matchup Preview
     </h3>
     <button
       v-if="preview === ''"
       @click="getPreview"
-      class="text-gray-50 mt-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base sm:text-sm w-full sm:w-auto px-3 sm:px-3 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      class="w-full px-3 py-2 mt-3 text-base font-medium text-center bg-blue-700 rounded-lg text-gray-50 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 sm:text-sm sm:w-auto sm:px-3 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
     >
       Generate
     </button>
@@ -80,7 +102,7 @@ const promptData = computed(() => {
       <p>{{ preview }}</p>
       <p
         v-if="preview !== 'Unable to generate preview. Please try again later.'"
-        class="text-xs text-gray-500 dark:text-gray-400 mt-1"
+        class="mt-1 text-xs text-gray-500 dark:text-gray-400"
       >
         Generated using GPT-4.1-mini. Information provided may not always be
         accurate.
