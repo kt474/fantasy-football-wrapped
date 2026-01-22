42b735a5029ac9b577046a9745346008fbca422a
kt474 <kt474@cornell.edu>
2026-01-21 21:51:37 -0500

    Initial ios commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ffwrapped',
+  appName: 'ffwrapped',
+  webDir: 'dist'
+};
+
+export default config;
