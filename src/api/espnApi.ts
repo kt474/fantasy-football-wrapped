d30c8e2f3854e91a072dd3551f0bf9ccf6ce27d0
kt474 <kt474@cornell.edu>
2026-03-17 15:39:01 -0400

    Scaffold endpoints

--- src/api/espnApi.ts
+++ src/api/espnApi.ts
@@ -1,9 +1,74 @@
 const ESPN_BASE_URL = "https://lm-api-reads.fantasy.espn.com";
 
-export const getData = async (season: string, league_id: string) => {
+export const getLeagueData = async (season: string, league_id: string) => {
   try {
     const response = await fetch(
-      `${ESPN_BASE_URL}/apis/v3/games/ffl/seasons/${season}/segments/0/leagues/${league_id}`
+      `${ESPN_BASE_URL}/apis/v3/games/ffl/seasons/${season}/segments/0/leagues/${league_id}?view=mSettings`
+    );
+    const json = await response.json();
+    return {
+      leagueName: json.settings.name,
+      currentWeek: json.status.currentMatchupPeriod,
+      draftDetail: json.draftDetail,
+      waivers: json.settings.acquisitionSettings,
+      draftSettings: json.settings.draftSettings,
+      isPublic: json.settings.isPublic,
+      scoringSettings: json.settings.scoringSettings,
+    };
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const getTeamData = async (season: string, league_id: string) => {
+  try {
+    const response = await fetch(
+      `${ESPN_BASE_URL}/apis/v3/games/ffl/seasons/${season}/segments/0/leagues/${league_id}?view=mTeam`
+    );
+    const json = await response.json();
+    return {
+      users: json.members,
+      rosters: json.teams,
+    };
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const getRosterData = async (season: string, league_id: string) => {
+  try {
+    const response = await fetch(
+      `${ESPN_BASE_URL}/apis/v3/games/ffl/seasons/${season}/segments/0/leagues/${league_id}?view=mRoster`
+    );
+    const json = await response.json();
+    return {
+      users: json.members,
+      currentWeek: json.status.currentMatchupPeriod,
+      finalWeek: json.finalScoringPeriod,
+      previousSeasons: json.previousSeasons,
+      rosters: json.teams,
+    };
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const getMatchups = async (season: string, league_id: string) => {
+  try {
+    const response = await fetch(
+      `${ESPN_BASE_URL}/apis/v3/games/ffl/seasons/${season}/segments/0/leagues/${league_id}?view=mMatchupScore`
+    );
+    const json = await response.json();
+    return json.schedule;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const getDraftData = async (season: string, league_id: string) => {
+  try {
+    const response = await fetch(
+      `${ESPN_BASE_URL}/apis/v3/games/ffl/seasons/${season}/segments/0/leagues/${league_id}?view=mDraftDetail`
     );
     return response.json();
   } catch (e) {
--- src/components/espn/EspnTest.vue
+++ src/components/espn/EspnTest.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { getData } from "@/api/espnApi";
+import { getLeagueData } from "@/api/espnApi";
 import Button from "../ui/button/Button.vue";
 
 const data = ref();
 
 const testApi = async () => {
-  data.value = await getData("2025", "2127");
+  data.value = await getLeagueData("2025", "2127");
 };
 </script>
 <template>
-  <div>
+  <div class="p-4">
     <Button @click="testApi()">Fetch Data</Button>
     <div>
       {{ data }}
