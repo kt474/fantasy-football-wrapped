89cfef909d9c8693ad1e8f4b2237e4c35f4ae483
kt474 <kt474@cornell.edu>
2026-07-05 15:47:11 -0400

    Use pngs for pwa icon

--- vite.config.ts
+++ vite.config.ts
@@ -20,19 +20,19 @@ export default defineConfig(({ mode }) => ({
         display: "standalone",
         icons: [
           {
-            src: "192.webp",
+            src: "192.png",
             sizes: "192x192",
-            type: "image/webp",
+            type: "image/png",
           },
           {
             src: "512.webp",
             sizes: "512x512",
-            type: "image/webp",
+            type: "image/png",
           },
           {
-            src: "512.webp",
+            src: "512.png",
             sizes: "512x512",
-            type: "image/webp",
+            type: "image/png",
             purpose: "any maskable",
           },
         ],
