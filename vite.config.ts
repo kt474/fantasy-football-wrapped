c2ea7f442df72d8e8b5b936d1cd0f759ca7c15b8
kt474 <kt474@cornell.edu>
2026-07-13 21:37:57 -0400

    Fix mobile safari lag issues

--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,7 @@ export default defineConfig(({ mode }) => ({
         ],
       },
       workbox: {
+        cleanupOutdatedCaches: true,
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
         runtimeCaching: [
           {
